8a86873d47d48e2f7ec925c8007a29cb8c11048b
Siwa Khongsuphap <siwa@hospital-os.com>
2021-10-18 10:35:15 +0700

    init project

---
+++ config/importmap.rb
@@ -0,0 +1,8 @@
+# Pin npm packages by running ./bin/importmap
+
+pin "application", preload: true
+pin "@hotwired/turbo-rails", to: "turbo.js"
+pin "@hotwired/stimulus", to: "stimulus.js"
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin_all_from "app/javascript/controllers", under: "controllers"
+pin "@rails/request.js", to: "https://ga.jspm.io/npm:@rails/request.js@0.0.5/src/index.js"
